9470f1714dd68551a99f530839c84b5cd44b6d16
Thiago Oliveira Vieira <thiago.ov@hotmail.com>
2025-11-24 17:30:21 -0300

    starting the project with laravel+inertia+vue+tailwaildcss 24/11/2025

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,15 @@
+<script setup>
+defineProps({
+    user: {
+        type: String,
+        default: "Josh",
+    },
+});
+</script>
+
+<template>
+    <h1>About</h1>
+    <p>{{ user }}</p>
+</template>
+
+<style scoped></style>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+
+<template>
+    <h1 class="text-3xl font-bold">Home</h1>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab provident
+        iusto debitis. Eveniet alias eos deserunt, corrupti, odit ut delectus
+        atque illo a possimus maiores, sequi officia molestias impedit qui. Ab
+        quidem dolore dolores, libero omnis quas saepe quod nostrum tempora
+        minima non obcaecati dolorem natus a odit et, maiores sunt! Veniam
+        voluptates facilis corrupti ipsum! Harum eligendi repudiandae explicabo.
+        Doloremque accusamus, velit aliquam laborum nulla necessitatibus, quo
+        repellat eligendi quia nihil veritatis dolore corporis sed quaerat
+        voluptatum ut? Fuga ab inventore nostrum expedita magnam tenetur
+        voluptas doloremque repellendus accusantium?
+    </p>
+    <p>
+        Eligendi magnam consequatur ad voluptas dolorum at, dignissimos quas.
+        Error, quam nihil similique omnis dignissimos deleniti, perferendis
+        officiis minima quidem reprehenderit laborum inventore fugit alias
+        aliquid minus consequatur ut dicta. Accusamus veritatis ducimus labore
+        laboriosam alias nihil corporis consequatur inventore amet cupiditate.
+        Optio, mollitia, numquam saepe quidem ullam unde molestias dolorum
+        aperiam suscipit explicabo deleniti sint veritatis quis voluptatibus
+        nemo!
+    </p>
+</template>
+
+<style scoped></style>
